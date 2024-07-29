8b69e78efd2cc20dd01dfe81fc031b39022bbcee
Marc Stephen <marcstephen.stp@gmail>
2024-07-29 22:12:49 +0530

    built a reponsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
 import "./App.css";
-import { Button } from "@chakra-ui/react";
+import { Button, Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-  return <Button>Marc</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav""main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav" bg={"coral"}>
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg={"gold"}>
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg={"coral"}>
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
